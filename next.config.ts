2e17526a4f34bcad93016794437a66b21f0f63cc
Tan Phong <s3877819@rmit.edu.vn>
2025-07-14 23:57:26 +0700

    export

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,15 @@
 import type { NextConfig } from "next";
 
 const repoName = "pongnatel";
-const isExport = process.env.NEXT_EXPORT === "true";
 
 const nextConfig: NextConfig = {
   basePath: "/" + repoName,
   assetPrefix: "/" + repoName + "/",
   devIndicators: false,
-  ...(isExport
-    ? {
-        output: "export",
-        trailingSlash: true,
-        images: { unoptimized: true },
-      }
-    : {
-        // Server mode: do not set output, trailingSlash, or images.unoptimized
-      }),
+
+  output: "export",
+  trailingSlash: true,
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
